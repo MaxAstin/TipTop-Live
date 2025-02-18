6c037db0002e146862220eeda4827be5b8781d4a
v.podkopaev <max.astin@yandex.ru>
2025-02-18 16:07:15 +0200

    fix prices mapping

--- app/src/main/kotlin/com/bunbeauty/tiptoplive/features/billing/mapper/ProductDetailsMapper.kt
+++ app/src/main/kotlin/com/bunbeauty/tiptoplive/features/billing/mapper/ProductDetailsMapper.kt
@@ -32,59 +32,86 @@ fun ProductDetails.subscriptionToProduct(percent: Int): Product? {
     )
 }
 
-private fun String.calculatePreviousPrice(percent: Int): String {
-    val currency = currency()
-    val decimalPart = decimalPart()
-    var price = toBigBigDecimal()
-    if (price.toInt() == 0) {
-        price = BigDecimal(1)
-    }
-    val previousPrice = price
-        .multiply(BigDecimal(100))
-        .divide(
-            BigDecimal(100 - percent),
-            0,
-            RoundingMode.HALF_UP
-        )
-        .toInt()
+private fun String.calculatePreviousPrice(percent: Int): String? {
+    return try {
+        val decimalPart = decimalPart()
+        var price = toBigBigDecimal()
+        if (price.toInt() == 0) {
+            price = BigDecimal(1)
+        }
+        val previousPrice = price
+            .multiply(BigDecimal(100))
+            .divide(
+                BigDecimal(100 - percent),
+                0,
+                RoundingMode.HALF_UP
+            )
+            .toInt()
 
-    return "$currency$previousPrice$decimalPart"
+        "${getPrefix()}$previousPrice$decimalPart${getPostfix()}"
+    } catch (exception: Exception) {
+        null
+    }
 }
 
-private fun String.calculateDiscountPercent(percent: Int): Int {
-    val currentPrice = toBigBigDecimal()
-    val decimalPart = decimalPart()
-    val previousPrice = currentPrice.multiply(BigDecimal(100))
-        .divide(
-            BigDecimal(100 - percent),
-            0,
-            RoundingMode.HALF_UP
-        )
-        .toInt()
-    return 100 - currentPrice
-        .multiply(BigDecimal(100))
-        .divide(
-            BigDecimal("$previousPrice$decimalPart"),
-            0,
-            RoundingMode.HALF_UP
-        )
-        .toInt()
+private fun String.calculateDiscountPercent(percent: Int): Int? {
+    return try {
+        val currentPrice = toBigBigDecimal()
+        val decimalPart = decimalPart()
+            .replace(',', '.')
+        val previousPrice = currentPrice.multiply(BigDecimal(100))
+            .divide(
+                BigDecimal(100 - percent),
+                0,
+                RoundingMode.HALF_UP
+            )
+            .toInt()
+        100 - currentPrice
+            .multiply(BigDecimal(100))
+            .divide(
+                BigDecimal("$previousPrice$decimalPart"),
+                0,
+                RoundingMode.HALF_UP
+            )
+            .toInt()
+    } catch (exception: Exception) {
+        null
+    }
 }
 
 private fun String.toBigBigDecimal(): BigDecimal {
-    val number = replace(currency(), "")
-        .replace(",", "")
+    var number = replace(getPrefix(), "")
+        .replace(getPostfix(), "")
+    number = if (number.contains('.') || number.count { it == ',' } > 1) {
+        number.replace(",", "")
+    } else {
+        number.replace(',', '.')
+    }
     return BigDecimal(number)
 }
 
-private fun String.currency(): String {
-    return takeWhile { !it.isDigit() }
+private fun String.getPrefix(): String {
+    return takeWhile { char ->
+        !char.isDigit()
+    }
+}
+
+private fun String.getPostfix(): String {
+    return takeLastWhile { char ->
+        !char.isDigit()
+    }
 }
 
 private fun String.decimalPart(): String {
-    return split('.')
-        .getOrNull(1)
-        ?.let { decimal ->
-            ".$decimal"
-        }.orEmpty()
+    val lastDotIndex = lastIndexOf('.')
+    val lastCommaIndex = lastIndexOf(',')
+    val (index, divider) = if (lastDotIndex > lastCommaIndex) {
+        lastDotIndex to '.'
+    } else {
+        lastCommaIndex to ','
+    }
+
+    val digits = substring(index + 1, length)
+        .filter { it.isDigit() }
+    return "$divider$digits"
 }
